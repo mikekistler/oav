9b37d2627cc8dfaf2cd6c40f17436140c8d0116f
Mike Kistler <mikekistler@microsoft.com>
2022-10-12 08:03:27 -0500

    Test for FileLoader.isUnderFileRoot

---
+++ test/swagger/fileLoaderTests.ts
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+import assert from "assert";
+import { FileLoader, FileLoaderOption } from "../../lib/swagger/fileLoader";
+
+describe("FileLoader functions", () => {
+  describe("isFileUnderRoot", () => {
+    it("should check absolute paths correctly", () => {
+      const options: FileLoaderOption = {
+        fileRoot:
+          "/Users/mikekistler/Projects/Azure/oav/test/liveValidation/swaggers/specification",
+      };
+      const loader = new FileLoader(options);
+      const received = loader.isUnderFileRoot("Users/mikekistler/Projects/Azure/oav/readme.md");
+      assert.strictEqual(received, false);
+    });
+  });
+});
